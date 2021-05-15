5e70a694dd02dad8d5d997df44f1afd42791b09c
TimJonesB <tjones189325@gmail.com>
2021-05-15 16:00:10 -0500

    change to hdf5 c api for thread safety

--- src/h5_sandbox.cpp
+++ src/h5_sandbox.cpp
@@ -1,76 +1,57 @@
 #include <iostream>
-#include "H5Cpp.h"
 #include <string>
 #include <vector>
 #include <map>
-#include "h5_sandbox.h"
-const int NX = 900000;
-const int NX_SUB = 900000;
+#include "hdf5.h"
+const int NX = 10;
+const int NX_SUB = 5;
 const int RANK_OUT = 1;
+
+constexpr std::string_view FILENAME = "../data/outfile.h5";
+constexpr std::string_view DATASET = "label";
+const bool debug_bool = 1;
 int run_it() {
-    std::string filepath{"../data/outfile.h5"};
-    H5::H5File f{filepath, H5F_ACC_RDONLY};
-    H5::DataSet dataset = f.openDataSet("label");
-    H5T_class_t type_class = dataset.getTypeClass();
-    std::cout << "type_class: " <<  type_class << std::endl;
-    if( type_class == H5T_INTEGER ) {
+    int data_out[NX];
 
-        int data_out[NX];
-        for (int i = 0; i < NX; i++) {
-            data_out[i] = -3;
+    hid_t file = H5Fopen (std::string(FILENAME).c_str(), H5F_ACC_RDONLY, H5P_DEFAULT);
+    hid_t dataset = H5Dopen (file, std::string(DATASET).c_str(), H5P_DEFAULT);
+    hid_t datatype = H5Dget_type(dataset);
+    H5T_class_t dclass = H5Tget_class(datatype);
+    H5T_order_t order = H5Tget_order(datatype);
+    hsize_t size = H5Tget_size(datatype);
+    hid_t dataspace = H5Dget_space(dataset);
+
+    const int rank = H5Sget_simple_extent_ndims(dataspace);
+    hsize_t dims[rank];
+    hsize_t maxdims[rank];
+    int status = H5Sget_simple_extent_dims(dataspace, dims, maxdims);
+
+    hsize_t offset[1] = {0};
+    hsize_t count[1] = {NX_SUB};
+    status = H5Sselect_hyperslab(dataspace, H5S_SELECT_SET, offset, NULL, count, NULL);
+
+    hsize_t dimsm[1] = {NX};
+    hid_t memspace = H5Screate_simple(RANK_OUT, dimsm, NULL);
+    hsize_t offset_out[1] = {0};
+    hsize_t count_out[1] = {NX_SUB};
+    status = H5Sselect_hyperslab(memspace, H5S_SELECT_SET, offset_out, NULL, count_out, NULL);
+
+    status = H5Dread(dataset, H5T_NATIVE_INT, memspace, dataspace, H5P_DEFAULT, data_out);
+
+    if (debug_bool) {
+        if (dclass == H5T_INTEGER) {
+            std::cout << "Dataset has Integer Type" << std::endl;
         }
-        std::cout << "Data set has INTEGER type" << std::endl;
-        // Get the integer datatype
-        H5::IntType intype = dataset.getIntType();
-
-        // Get order of datatype and print message if it's a little endian.
-        H5std_string order_string;
-        H5T_order_t order = intype.getOrder(order_string);
-        std::cout << order_string << std::endl;
-
-        // Get size of the data element stored in file and print it.
-        size_t size = intype.getSize();
-        std::cout << "Data size is " << size << std::endl;
-
-        // Get dataspace of the dataset.
-        H5::DataSpace dataspace = dataset.getSpace();
-
-        // Get the number of dimensions in the dataspace.
-        int rank = dataspace.getSimpleExtentNdims();
-
-        // Get the dimension size of each dimension in the dataspace and display them.
-        hsize_t dims_out[1];
-        int ndims = dataspace.getSimpleExtentDims(dims_out, NULL);
-        std::cout << "ndims: " << ndims << std::endl;
-        std::cout << "rank " << rank << ", dimensions " << (unsigned long)(dims_out[0]) << std::endl;
-
-        // Define hyperslab in the dataset; implicitly giving strike and block NULL.
-        hsize_t offset[1];   // hyperslab offset in the file
-        hsize_t count[1];    // size of the hyperslab in the file
-        offset[0] = dims_out[0]-NX;
-        count[0] = NX_SUB;
-        dataspace.selectHyperslab(H5S_SELECT_SET, count, offset);
-
-        //  Define the memory dataspace.
-        hsize_t dimsm[1];              /* memory space dimensions */
-        dimsm[0] = NX;
-        H5::DataSpace memspace{RANK_OUT, dimsm};
-
-        // Define memory hyperslab.
-        hsize_t offset_out[1];   // hyperslab offset in memory
-        hsize_t count_out[1];    // size of the hyperslab in memory
-        offset_out[0] = 0;
-        count_out[0]  = NX_SUB;
-        memspace.selectHyperslab(H5S_SELECT_SET, count_out, offset_out);
-        
-        // Read data from hyperslab in the file into the hyperslab in memory and display the data.
-        dataset.read(data_out, H5::PredType::NATIVE_INT, memspace, dataspace );
-        
-
-        std::vector<int> v (data_out, data_out + (sizeof(data_out)/sizeof(data_out[0])));
+        if (order == H5T_ORDER_LE) {
+            std::cout << "Dataset has LE order" << std::endl;
+        }
+        std::cout << "nDims = " << dims[0] << std::endl;
+        std::cout << "Size is " << size << std::endl;
     }
-    std::cout << xxxx; 
-    std::cout <<"done fool" << std::endl;
+    H5Sclose(dataspace);
+    H5Tclose(datatype);
+    H5Dclose(dataset);
+    H5Fclose(file);
 
     return 0;
 }
